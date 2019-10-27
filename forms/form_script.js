5511fad6e1e5d6c77d2933f89db94556fdd2f0d8
NazarDs1 <55633052+NazarDs1@users.noreply.github.com>
2019-10-27 18:54:04 -0500

    Update form_script.js

--- forms/form_script.js
+++ forms/form_script.js
@@ -33,8 +33,8 @@ $("#btn_submit").on('click', function(ev){
   $.ajax({ // En data puedes utilizar un objeto JSON, un array o un query string
      //data: {"parametro1" : "valor1", "parametro2" : "valor2"}, //Cambiar a type: POST si necesario
      data:datform,
-     type: "GET", // Formato de datos que se espera en la respuesta
-     dataType: "json", // URL a la que se enviará la solicitud Ajax
+     type: "POST", // Formato de datos que se espera en la respuesta
+     dataType: "html", // URL a la que se enviará la solicitud Ajax
      url: "http://nazardesign.dx.am/php/login_regist.php", })
      .done(function( data, textStatus, jqXHR ) {
      $("#resp").html(data);
