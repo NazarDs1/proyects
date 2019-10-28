86dcc0527efdbd59afc61d6fd3ee984d5dfc3d0a
NazarDs1 <55633052+NazarDs1@users.noreply.github.com>
2019-10-28 10:11:35 -0500

    Update form_script.js

--- forms/form_script.js
+++ forms/form_script.js
@@ -34,7 +34,7 @@ $("#btn_submit").on('click', function(ev){
      //data: {"parametro1" : "valor1", "parametro2" : "valor2"}, //Cambiar a type: POST si necesario
      data:datform,
      xhrFields: {
-        withCredentials: false
+        withCredentials: true
      },
      crossDomain: true,
      type: "POST", // Formato de datos que se espera en la respuesta
