a1249135dfcd8a97e849b5a95d394dc08b26e738
NazarDs1 <55633052+NazarDs1@users.noreply.github.com>
2019-10-27 22:03:53 -0500

    Update form_script.js

--- forms/form_script.js
+++ forms/form_script.js
@@ -39,7 +39,8 @@ $("#btn_submit").on('click', function(ev){
      crossDomain: true,
      type: "POST", // Formato de datos que se espera en la respuesta
      dataType: "json", // URL a la que se enviará la solicitud Ajax
-     url: "http://nazardesign.dx.am/php/login_regist_cros.php", })
+     url: "http://nazardesign.dx.am/php/login_regist_cros.php" 
+  })
      .done(function( data, textStatus, jqXHR ) {
      $("#resp").html(data);
      $("#resp").slideDown(100);
